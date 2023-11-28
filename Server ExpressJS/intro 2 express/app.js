682c9982a11560059f0bc729a01bc5dbb7a2e661
Rundan53 <rundanonkar1999@gmail.com>
2023-11-28 15:19:37 +0530

    static pages

--- Server ExpressJS/intro 2 express/app.js
+++ Server ExpressJS/intro 2 express/app.js
@@ -1,18 +1,22 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const rootDir = require('./util/path.js')
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(rootDir,'public')))
 
 app.use(`/admin`,adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next) => {
-    res.status(404).send(`<h1>Page not found</h1>`)
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 //creates server and call listen method on server object using 'apply'
